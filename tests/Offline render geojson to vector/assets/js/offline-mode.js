4ecb6ed2567d64222c25be1ea258844fd5a55dd1
Yassine FERNANE <yassine.fernane@gmail.com>
2017-01-09 22:53:04 +0100

    Dessin vectoriel depuis fichier GeoJSON faute d'alternatives.

--- tests/Offline render geojson to vector/assets/js/offline-mode.js
+++ tests/Offline render geojson to vector/assets/js/offline-mode.js
@@ -1,23 +1,5 @@
-var xml = Fichier('.assets/data/map.osm');   
-        
-var geojson = osm2geo(xml);
-
-console.log(geojson);
-
-var blob = new Blob([geojson], {type: "application/json"});
-var url = URL.createObjectURL(blob);
-
-var a = document.createElement('a');
-a.download = "map.geojson";
-a.href = url;
-a.textContent = "Dl map.json"; 
-
 var width  = 600,
-	height = 400;
-			
-var map = L.map('mapid').setView([51.505, -0.09], 13);
-	
-L.tileLayer('./assets/Tiles/{z}/{x}/{y}.png', {maxzoom: 16}).addTo(map);
+	height = 400;		
 	
 var projection = d3.geo.mercator()
 					   .translate([0, 0])
@@ -30,9 +12,15 @@ var svg = d3.select("#mapid")
 			.append("svg")
 			.attr("width", width)
 			.attr("height", height)
-			.call(d3.behavior.zoom().scaleExtent([1, 20]).on("zoom", zoom))
-			.append("g")
-			.attr("class", "leaflet-zoom-hide");
+			.call(d3.behavior.zoom().scaleExtent([1, 20]).on("zoom", zoom)),
+			
+	g = svg.append("g")
+			.attr("class", "leaflet-zoom-hide"),
+			
+	tooltip = d3.select("body")
+				.append("div")
+				.attr("class", "tooltip");
+			
 			
 d3.json("./assets/data/regions.geojson", function(json) {			
 	var b = path.bounds( json ),
@@ -43,35 +31,28 @@ d3.json("./assets/data/regions.geojson", function(json) {
 		.scale(s)
 		.translate(t);
 				
-	svg.selectAll("path")
+	g.selectAll("path")
 		.data(json.features)
 		.enter()
 		.append("path")
+		.attr("id", function(d) { return d.properties.code})
+		.on("mouseover", function(d) { tooltip.html(d.properties.nom);})
+		.on("mouseout", function(d) {tooltip.html("");})
 		.attr("d", path)
 		.style("fill", "steelblue");	
+		
+	//Nom des villes
+	/*g.selectAll("labels")
+		.data(json.features)
+		.enter()
+		.append("text")
+		.attr("class", "labels")
+		.attr("transform", function(d) {return "translate(" + path.centroid(d) + ")";})
+		.attr("dy", ".35em")
+		.text(function(d) {return d.properties.nom;});*/
+		
 });
 
 function zoom() {
   svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-}
-
-function Fichier(fichier)
-{
-    if(window.XMLHttpRequest) 
-        obj = new XMLHttpRequest(); //Pour Firefox, Opera,...
-    else if(window.ActiveXObject) 
-        obj = new ActiveXObject("Microsoft.XMLHTTP"); //Pour Internet Explorer 
-    else 
-        return(false);
-
-    if (obj.overrideMimeType) 
-        obj.overrideMimeType("text/xml"); //Évite un bug de Safari
-
-    obj.open("GET", fichier, false);
-    obj.send(null);
-
-    if(obj.readyState == 4) 
-        return(obj.responseText);
-    else 
-        return(false);
 }
